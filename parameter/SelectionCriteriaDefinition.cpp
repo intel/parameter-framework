8d6e34b5fed9d64266cb9dae35677dd19eb641b6
Kevin Rocard <kevinx.rocard@intel.com>
2012-12-03 14:45:29 +0100

    PFW: Add log on criterion creation
    BZ: 71914
    
    Initial criterion state and posible values are needed for criterion
    coverage.
    
    Change-Id: Id6058376fc64eb4e523fe0badffc09a7f4238f6c
    Origin-Change-Id: I76c5cb596df1d20af48f0df03015c4be90cd5d96
    Signed-off-by: Kevin Rocard <kevinx.rocard@intel.com>
    Reviewed-on: http://android.intel.com:8080/81268
    Reviewed-by: Barthes, FabienX <fabienx.barthes@intel.com>
    Tested-by: Barthes, FabienX <fabienx.barthes@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- parameter/SelectionCriteriaDefinition.cpp
+++ parameter/SelectionCriteriaDefinition.cpp
@@ -37,6 +37,12 @@ string CSelectionCriteriaDefinition::getKind() const
 // Selection Criterion creation
 CSelectionCriterion* CSelectionCriteriaDefinition::createSelectionCriterion(const string& strName, const CSelectionCriterionType* pSelectionCriterionType)
 {
+    log("Creating %s selection criterion \"%s\" - possible states : %s",
+        pSelectionCriterionType->isTypeInclusive() ? "inclusive" : "exclusive",
+        strName.c_str(),
+        pSelectionCriterionType->listPossibleValues().c_str()
+        );
+
     CSelectionCriterion* pSelectionCriterion = new CSelectionCriterion(strName, pSelectionCriterionType);
 
     addChild(pSelectionCriterion);
