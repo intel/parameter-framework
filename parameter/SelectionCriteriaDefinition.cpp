a4e7b912aa20fe19dc3276221106e7c11e844deb
Kevin Rocard <kevinx.rocard@intel.com>
2012-12-14 15:33:28 +0100

    PFW: Revert "PFW: Add log on criterion creation"
    BZ: 77959
    
    This reverts commit with change-Id: I76c5cb596df1d20af48f0df03015c4be90cd5d96.
    Logging the criterion should be done on PFW start. If not it
    requires the criterion type not to change until start.
    
    Change-Id: If3d6faeffe72938bc920e28f6dfccccb530a1dee
    Signed-off-by: Kevin Rocard <kevinx.rocard@intel.com>
    Reviewed-on: http://android.intel.com:8080/81830
    Reviewed-by: Centelles, Sylvain <sylvain.centelles@intel.com>
    Tested-by: Dixon, CharlesX <charlesx.dixon@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- parameter/SelectionCriteriaDefinition.cpp
+++ parameter/SelectionCriteriaDefinition.cpp
@@ -37,12 +37,6 @@ string CSelectionCriteriaDefinition::getKind() const
 // Selection Criterion creation
 CSelectionCriterion* CSelectionCriteriaDefinition::createSelectionCriterion(const string& strName, const CSelectionCriterionType* pSelectionCriterionType)
 {
-    log_info("Creating %s selection criterion \"%s\" - possible states : %s",
-        pSelectionCriterionType->isTypeInclusive() ? "inclusive" : "exclusive",
-        strName.c_str(),
-        pSelectionCriterionType->listPossibleValues().c_str()
-        );
-
     CSelectionCriterion* pSelectionCriterion = new CSelectionCriterion(strName, pSelectionCriterionType);
 
     addChild(pSelectionCriterion);
