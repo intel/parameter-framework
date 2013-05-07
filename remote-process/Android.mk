c6de01abbc5539b4eaa7b16f8882f2f22994aaf0
Kevin Rocard <kevinx.rocard@intel.com>
2013-05-07 17:29:53 +0200

    [remote-process] Add support for host build
    BZ: 107991
    
    In order to generate setting xml at build time,
    one need to build and run PFW environement on host.
    
    Add an host compilation target.
    
    Change-Id: Id3e94c799b98d3414969a184ab8487051d2c2c60
    Signed-off-by: Kevin Rocard <kevinx.rocard@intel.com>
    Reviewed-on: http://android.intel.com:8080/107270
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Gonzalve, Sebastien <sebastien.gonzalve@intel.com>
    Tested-by: Dixon, CharlesX <charlesx.dixon@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- remote-process/Android.mk
+++ remote-process/Android.mk
@@ -1,32 +1,59 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+####################
+# Common definitions
 
-LOCAL_MODULE_PATH :=
-
-
-LOCAL_SRC_FILES := \
+COMMON_SRC_FILES := \
         main.cpp
 
+COMMON_MODULE := remote-process
+COMMON_MODULE_TAGS := optional
 
-LOCAL_MODULE := remote-process
+COMMON_ERROR_FLAGS := -Wno-non-virtual-dtor
 
-LOCAL_MODULE_TAGS := optional
+COMMON_C_INCLUDES := \
+    $(LOCAL_PATH)/../remote-processor/
 
-TARGET_ERROR_FLAGS += -Wno-non-virtual-dtor
+COMMON_SHARED_LIBRARIES := libremote-processor
+
+#############################
+# Target build
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(COMMON_SRC_FILES)
+
+LOCAL_MODULE := $(COMMON_MODULE)
+LOCAL_MODULE_TAGS := $(COMMON_MODULE_TAGS)
+
+TARGET_ERROR_FLAGS += $(COMMON_ERROR_FLAGS)
 
 LOCAL_C_INCLUDES += \
-        external/stlport/stlport/ \
-        bionic/libstdc++ \
-        bionic/
+    $(COMMON_C_INCLUDES) \
+    external/stlport/stlport/ \
+    bionic/libstdc++ \
+    bionic/
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../remote-processor/
+LOCAL_SHARED_LIBRARIES := $(COMMON_SHARED_LIBRARIES) libstlport
 
-LOCAL_SHARED_LIBRARIES := libstlport libicuuc libremote-processor
-LOCAL_STATIC_LIBRARIES :=
-
-LOCAL_LDLIBS +=
-LOCAL_LDFLAGS +=
 
 include $(BUILD_EXECUTABLE)
 
+##############################
+# Host build
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(COMMON_SRC_FILES)
+
+LOCAL_MODULE := $(COMMON_MODULE)_host
+LOCAL_MODULE_TAGS := $(COMMON_MODULE_TAGS)
+
+TARGET_ERROR_FLAGS += $(COMMON_ERROR_FLAGS)
+
+LOCAL_C_INCLUDES += \
+    $(COMMON_C_INCLUDES)
+
+LOCAL_SHARED_LIBRARIES := $(COMMON_SHARED_LIBRARIES)
+
+include $(BUILD_HOST_EXECUTABLE)
