3885ae46991f46250a84edc8f138e05a200364b7
Kevin Rocard <kevinx.rocard@intel.com>
2013-05-13 18:35:01 +0200

    [test-platform] Add support for host build
    BZ: 107991
    
    In order to generate setting xml at build time,
    one need to build and run PFW environement on host.
    
    Add an host compilation target.
    
    Change-Id: I8f736652f34c412dab20c185098abb4319bb001f
    Signed-off-by: Kevin Rocard <kevinx.rocard@intel.com>
    Reviewed-on: http://android.intel.com:8080/107406
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Gonzalve, Sebastien <sebastien.gonzalve@intel.com>
    Tested-by: Dixon, CharlesX <charlesx.dixon@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- test/test-platform/Android.mk
+++ test/test-platform/Android.mk
@@ -1,33 +1,59 @@
 LOCAL_PATH:= $(call my-dir)
 
-include $(CLEAR_VARS)
+####################
+# Common definitions
 
-
-LOCAL_SRC_FILES:= \
+COMMON_SRC_FILES := \
     main.cpp \
     TestPlatform.cpp
 
+COMMON_MODULE := test-platform
+COMMON_MODULE_TAGS := optional
 
-LOCAL_MODULE:= test-platform
+COMMON_ERROR_FLAGS := -Wno-non-virtual-dtor
 
-LOCAL_MODULE_TAGS := optional
-
-# Ignore non virtual destructor error
-TARGET_ERROR_FLAGS += -Wno-non-virtual-dtor
-
-LOCAL_C_INCLUDES += \
-    external/stlport/stlport \
-    bionic/libstdc++ \
-    bionic
-
-LOCAL_C_INCLUDES += \
+COMMON_C_INCLUDES := \
     $(LOCAL_PATH)/../../parameter \
     $(LOCAL_PATH)/../../remote-processor/
 
-LOCAL_SHARED_LIBRARIES := \
-    libparameter \
-    libremote-processor \
-    libstlport
+COMMON_SHARED_LIBRARIES := libparameter libremote-processor
+
+#############################
+# Target build
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= $(COMMON_SRC_FILES)
+
+LOCAL_MODULE:= $(COMMON_MODULE)
+LOCAL_MODULE_TAGS := $(COMMON_MODULE_TAGS)
+
+TARGET_ERROR_FLAGS += $(COMMON_ERROR_FLAGS)
+
+LOCAL_C_INCLUDES += \
+    $(COMMON_C_INCLUDES) \
+    external/stlport/stlport/ \
+    bionic/libstdc++ \
+    bionic/
+
+LOCAL_SHARED_LIBRARIES := $(COMMON_SHARED_LIBRARIES) libstlport
 
 include $(BUILD_EXECUTABLE)
 
+##############################
+# Host build
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= $(COMMON_SRC_FILES)
+
+LOCAL_MODULE:= $(COMMON_MODULE)
+LOCAL_MODULE_TAGS := $(COMMON_MODULE_TAGS)
+
+TARGET_ERROR_FLAGS += $(COMMON_ERROR_FLAGS)
+
+LOCAL_C_INCLUDES += $(COMMON_C_INCLUDES)
+
+LOCAL_SHARED_LIBRARIES := $(COMMON_SHARED_LIBRARIES)
+
+include $(BUILD_HOST_EXECUTABLE)
