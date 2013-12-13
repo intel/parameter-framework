cee2d6bf92a30111b643e1222dcb64f4d116f724
Francois Gaffie <francois.gaffie@intel.com>
2013-12-13 11:08:51 +0100

    Add export include directive
    BZ: 156679
    
    Due to split of plugins and core, and to avoid hard coded path,
    needs to add an include directive in utility component makefile.
    
    Change-Id: Ifbe6bc2b9ea692cd482f1c6934f9eb54d16eae68
    Signed-off-by: Francois Gaffie <francois.gaffie@intel.com>

--- utility/Android.mk
+++ utility/Android.mk
@@ -1,3 +1,24 @@
+# INTEL CONFIDENTIAL
+#
+# Copyright 2013 Intel Corporation All Rights Reserved.
+# The source code contained or described herein and all documents
+# related to the source code ("Material") are owned by Intel
+# Corporation or its suppliers or licensors. Title to the Material
+# remains with Intel Corporation or its suppliers and licensors. The
+# Material contains trade secrets and proprietary and confidential
+# information of Intel or its suppliers and licensors. The Material is
+# protected by worldwide copyright and trade secret laws and treaty
+# provisions. No part of the Material may be used, copied, reproduced,
+# modified, published, uploaded, posted, transmitted, distributed, or
+# disclosed in any way without Intel's prior express written permission.
+#
+# No license under any patent, copyright, trade secret or other
+# intellectual property right is granted to or conferred upon you by
+# disclosure or delivery of the Materials, either expressly, by
+# implication, inducement, estoppel or otherwise. Any license under
+# such intellectual property rights must be express and approved by
+# Intel in writing.
+
 LOCAL_PATH := $(call my-dir)
 
 ####################
@@ -23,6 +44,8 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(common_src_files)
 
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+
 LOCAL_MODULE := $(common_module)
 LOCAL_MODULE_TAGS := $(common_module_tags)
 
@@ -40,6 +63,8 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(common_src_files)
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 
 LOCAL_MODULE := $(common_module)_host
 LOCAL_MODULE_TAGS := $(common_module_tags)
