5c61b128907d04b3e7f49008b4f1bfc520afdbc9
Jules Clero <julesx.clero@intel.com>
2015-02-26 15:57:00 +0100

    Remove unused AutoLog header from SelectionCriterion class
    Signed-off-by: Jules Clero <julesx.clero@intel.com>

--- parameter/SelectionCriterion.cpp
+++ parameter/SelectionCriterion.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2014, Intel Corporation
+ * Copyright (c) 2011-2015, Intel Corporation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -29,7 +29,6 @@
  */
 
 #include "SelectionCriterion.h"
-#include "AutoLog.h"
 
 #define base CElement
 
