977c4556c2ce85cc658f4709ae0bec9326547126
David Brageul <david.brageul@intel.com>
2015-04-30 15:29:37 +0200

    Android: Do not forbid clang compilation
    Change-Id: Ic94bb62ecd7e7e0fadb067d8bda3febb700d5fc6
    Signed-off-by: Sebastien Gonzalve <sebastien.gonzalve@intel.com>
    Signed-off-by: David Wagner <david.wagner@intel.com>
    Signed-off-by: David Brageul <david.brageul@intel.com>

--- parameter/Android.mk
+++ parameter/Android.mk
@@ -160,7 +160,6 @@ LOCAL_STATIC_LIBRARIES := libxmlserializer libpfw_utility libxml2
 
 LOCAL_REQUIRED_MODULES := libremote-processor
 
-LOCAL_CLANG := false
 include external/stlport/libstlport.mk
 include $(BUILD_SHARED_LIBRARY)
 
@@ -190,5 +189,4 @@ LOCAL_STATIC_LIBRARIES := libxmlserializer_host libpfw_utility_host libxml2
 
 LOCAL_LDLIBS += -ldl
 
-LOCAL_CLANG := false
 include $(BUILD_HOST_SHARED_LIBRARY)
