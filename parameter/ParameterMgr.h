4fa9af01f223e8a2613d332cb69b17c3e7bc1023
David Wagner <david.wagner@intel.com>
2015-05-28 18:30:48 +0200

    Version 2.5.0
    Signed-off-by: David Wagner <david.wagner@intel.com>

--- parameter/ParameterMgr.h
+++ parameter/ParameterMgr.h
@@ -90,8 +90,8 @@ class CParameterMgr : private CElement
     };
     // Version
     static const uint32_t guiEditionMajor = 0x2;
-    static const uint32_t guiEditionMinor = 0x4;
-    static const uint32_t guiRevision = 0x3;
+    static const uint32_t guiEditionMinor = 0x5;
+    static const uint32_t guiRevision = 0x0;
 
     // Parameter handle friendship
     friend class CParameterHandle;
