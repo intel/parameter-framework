7d4439c563f10c4ea26cea7d6603978013c98782
Sebastien Gonzalve <sebastien.gonzalve@intel.com>
2015-08-28 17:46:04 +0200

    move commands into where it bellongs
    The previous patch reworks the way the server gets the command handler.
    
    The command handler can now be created later as a temporary variable.
    
    Signed-off-by: Sebastien Gonzalve <sebastien.gonzalve@intel.com>

--- test/test-platform/TestPlatform.h
+++ test/test-platform/TestPlatform.h
@@ -149,9 +149,6 @@ private:
     // Logger
     CParameterMgrPlatformConnectorLogger* _pParameterMgrPlatformConnectorLogger;
 
-    // Command Handler
-    CCommandHandler* _pCommandHandler;
-
     // Remote Processor Server
     CRemoteProcessorServer* _pRemoteProcessorServer;
 };
