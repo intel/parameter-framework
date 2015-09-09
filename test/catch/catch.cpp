619bb9d8d9668679b64587e535717844b930e680
Kevin Rocard <kevin.rocard@intel.com>
2015-09-10 00:32:55 +0200

    Create a catch library to avoid copy paste
    Factorize the catch dependancy and accelerate build
    by creating a cacth library.
    
    See: https://github.com/philsquared/Catch/blob/master/docs/slow-compiles.md
    
    Signed-off-by: Kevin Rocard <kevin.rocard@intel.com>

--- test/catch/catch.cpp
+++ test/catch/catch.cpp
@@ -28,5 +28,8 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/** Accelerate test build by not recompiling catch when a test change.
+ * See: https://github.com/philsquared/Catch/blob/master/docs/slow-compiles.md
+ */
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
