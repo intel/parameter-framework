749e6654ebf547b844d24551309b991b3488353c
Mohamed LAHMADI <mohamedx.lahmadi@intel.com>
2017-02-03 12:11:23 +0100

    Fix KW issues
    Klocwork has reported 2 critical issues in utility/posix/DynamicLibrary.cpp:
    
    Array '(dlError!=nullptr)?dlError:"unknown dlopen error"' of size 21 may use index value(s) 21..INT_MAX
    utility/posix/DynamicLibrary.cpp: | DynamicLibrary()
    Code: ABV.GENERAL | Severity: Critical(1) | State: Existing | Status: Analyze
    
    Array '(dlError!=nullptr)?dlError:"unknown dlsym error"' of size 20 may use index value(s) 20..INT_MAX
    File: utility/posix/DynamicLibrary.cpp: | osGetSymbol()
    Code: ABV.GENERAL | Severity: Critical(1) | State: Existing | Status: Analyze
    
    Signed-off-by: Mohamed LAHMADI <mohamedx.lahmadi@intel.com>

--- utility/posix/DynamicLibrary.cpp
+++ utility/posix/DynamicLibrary.cpp
@@ -44,7 +44,8 @@ DynamicLibrary::DynamicLibrary(const std::string &path) : _path(osSanitizePathNa
     if (_handle == nullptr) {
 
         const char *dlError = dlerror();
-        throw std::runtime_error((dlError != nullptr) ? dlError : "unknown dlopen error");
+        static const char *unknowDlopenError = "unknown dlopen error";
+        throw std::runtime_error((dlError != nullptr) ? dlError : unknowDlopenError);
     }
 }
 
@@ -60,7 +61,8 @@ void *DynamicLibrary::osGetSymbol(const std::string &symbol) const
     if (sym == nullptr) {
 
         const char *dlError = dlerror();
-        throw std::runtime_error((dlError != nullptr) ? dlError : "unknown dlsym error");
+        static const char *unknowDlsymError = "unknown dlsym error";
+        throw std::runtime_error((dlError != nullptr) ? dlError : unknowDlsymError);
     }
 
     return sym;
